973ffdf5daa966e3ff9dd87ad3b773e41a21fd41
fauzan_abdussalam <fauzan@3trust.com>
2020-11-27 21:15:45 +0700

    Initial Commit

---
+++ src/quiz/if10k/pkg10119901/fauzanmuhammadabdussalam/CustomerInvoice.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiz.if10k.pkg10119901.fauzanmuhammadabdussalam;
+
+/**
+ *
+ * @author User
+ */
+public interface CustomerInvoice 
+{
+    public String currentTime();
+}
